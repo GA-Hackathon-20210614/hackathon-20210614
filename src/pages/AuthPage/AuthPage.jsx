60a65b2e819fd102a8f4ec0ec60d094dae9057b9
Trong Nguyen <superonion@Trongs-Air.fios-router.home>
2021-06-14 17:16:10 -0400

    Adds removed AuthPage

---
+++ src/pages/AuthPage/AuthPage.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './AuthPage.css';
+import LoginForm from '../../components/LoginForm/LoginForm';
+import SignUpForm from '../../components/SignUpForm/SignUpForm';
+
+export default function AuthPage({ setUser }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  return (
+    <main className="AuthPage">
+      <div>
+        <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'SIGN UP' : 'LOG IN'}</h3>
+      </div>
+      {showLogin ? <LoginForm setUser={setUser} /> : <SignUpForm setUser={setUser} />}
+    </main>
+  );
+}
