8d1c56097645d69e5eea59b9f00c74c031388834
John Teckemeyer <johnteckemeyer@gmail.com>
2021-06-14 13:21:18 -0700

    Create the classes-api service module

--- src/utilities/classes-api.js
+++ src/utilities/classes-api.js
@@ -4,20 +4,20 @@ const BASE_URL = '/api/classes';
 
 export function getAll() {
     return sendRequest(BASE_URL);
-  }
+}
 
 export function getById(id) {
-  return sendRequest(`${BASE_URL}/:${id}`);
+    return sendRequest(`${BASE_URL}/:${id}`);
 }
 
 export function updateOne(classData) {
-  return sendRequest(`${BASE_URL}/:${classData.id}`, 'PUT');
+    return sendRequest(`${BASE_URL}/:${classData.id}`, 'PUT', classData);
 }
 
 export function deleteOne(id) {
-  return sendRequest(`${BASE_URL}/:${id}`, 'DELETE');
+    return sendRequest(`${BASE_URL}/:${id}`, 'DELETE');
 }
 
-export function checkToken() {
-  return sendRequest(`${BASE_URL}/check-token`);
+export function create(classData) {
+    return sendRequest(BASE_URL, 'POST', classData)
 }
