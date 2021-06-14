f49ab55cff57b50b9ca8d0ce5539997a0d66c01b
Andrew8bit <Andrewbith@gmail.com>
2021-06-14 14:27:46 -0700

    [development] added blank files, students, class, assignments, attendance

---
+++ routes/api/students.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const router = express.Router();
+// const usersCtrl = require('../../controllers/api/users');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
