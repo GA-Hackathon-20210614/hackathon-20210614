a550361fcf3238ed252aaf232e0f8b0263a01c7f
Andrew8bit <Andrewbith@gmail.com>
2021-06-15 14:37:31 -0700

    [development] stash

--- controllers/api/classes.js
+++ controllers/api/classes.js
@@ -142,9 +142,8 @@ async function remove(req, res) {
 async function getAssignment(req, res) {
   const { assignment_id } = req.params;
   try {
-    console.log('hit')
+    
     const targetClass = await Class.find({ 'assignments._id': `${assignment_id}` }, {'assignments.$': 1});
-    console.log('fire')
     console.log(targetClass)
     res.json({success: true, targetClass})
 
