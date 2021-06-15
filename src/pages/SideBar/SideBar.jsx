815d425cdeabbcd5264086c1e1cae3d15022a2a5
Susana Martins <scmartins87@gmail.com>
2021-06-15 14:32:57 -0700

    assignment icon added to sidbar nav

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import DashboardPic from "../../images/dashboard.svg";
+import AssignmentPic from "../../images/assignment.svg";
 
 
 function SideBar(props) {
@@ -10,7 +11,7 @@ function SideBar(props) {
             <h1>sign in user name</h1>
             
             <Button variant="contaned" color="primary"><img src={DashboardPic} /> Dashboard</Button>
-            <button>Gradebook</button>
+            <Button variant="contaned" color="secondary"><img src={AssignmentPic} /> Gradebook</Button>
             <h3>Settings</h3>
             <button>Main Settings</button>
         </div>
