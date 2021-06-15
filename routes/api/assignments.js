f8f7bd987f870b65aeb9d48c97761975730248cd
huiylu <74116599+huiylu@users.noreply.github.com>
2021-06-15 09:45:43 -0700

    Checking out assignments

--- routes/api/assignments.js
+++ routes/api/assignments.js
@@ -9,6 +9,6 @@ router.get('/assignments', assignmentsCtrl.index);
 // UPDATE assignment
 router.put('/assignments/:id', assignmentsCtrl.update);
 // DELETE assignments
-router.delete('/assignments/:id', assignmentsCtrl.deleteAssignment);
+router.delete('/assignments/:id', assignmentsCtrl.delete);
 
 module.exports = router;
