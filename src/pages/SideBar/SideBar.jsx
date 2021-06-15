0b7b040ccdfd79ee81cf02419e6e2f7e1780c98d
Susana Martins <scmartins87@gmail.com>
2021-06-15 14:04:34 -0700

    test material ui button

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import "material-ui-sass";
+import Button from '@material-ui/core/Button';
+
 
 function SideBar(props) {
     return (
         <div>
             <img></img>
             <h1>sign in user name</h1>
-            <button>Dashboard</button>
+            <Button variant="contaned" color="primary">Dashboard</Button>
             <button>Gradebook</button>
             <h3>Settings</h3>
             <button>Main Settings</button>
