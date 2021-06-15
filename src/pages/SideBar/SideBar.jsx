f67ea6913a2af19b14b9e9fc372bfd31b11f3538
Susana Martins <scmartins87@gmail.com>
2021-06-15 14:27:57 -0700

    dashboard icon added to sidbar nav

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
+import DashboardPic from "../../images/dashboard.svg";
 
 
 function SideBar(props) {
@@ -7,7 +8,8 @@ function SideBar(props) {
         <div>
             <img></img>
             <h1>sign in user name</h1>
-            <Button variant="contaned" color="primary">Dashboard</Button>
+            
+            <Button variant="contaned" color="primary"><img src={DashboardPic} /> Dashboard</Button>
             <button>Gradebook</button>
             <h3>Settings</h3>
             <button>Main Settings</button>
