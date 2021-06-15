d2e8bdbb4b9c1a2412789f87c44138830f5882b0
Susana Martins <scmartins87@gmail.com>
2021-06-15 15:19:50 -0700

    added settings icon

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import DashboardPic from "../../images/dashboard.svg";
-import AssignmentPic from "../../images/assignment.svg";
+import DashboardIcon from "../../images/dashboard.svg";
+import AssignmentIcon from "../../images/assignment.svg";
+import SettingsIcon from "../../images/settings.svg";
 
 
 function SideBar(props) {
     return (
         <div>
             <img></img>
-            <h1>User</h1>
+            <h1>UserName</h1>
             
-            <Button variant="contaned" color="primary"><img src={DashboardPic} /> Dashboard</Button>
-            <Button variant="contaned" color="secondary"><img src={AssignmentPic} /> Gradebook</Button>
-            <h6>Settings</h6>
+            <Button variant="contained"><img src={DashboardIcon} /> Dashboard</Button>
+            <Button variant="contained"><img src={AssignmentIcon} /> Gradebook</Button>
+            <Button variant="contained"><img src={SettingsIcon} /> Settings</Button>
             <button>Main Settings</button>
         </div>
     );
