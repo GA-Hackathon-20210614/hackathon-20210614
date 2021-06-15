0fc6232bfb56b4d1b54957861ef3b790af4ed880
Susana Martins <scmartins87@gmail.com>
2021-06-15 09:57:17 -0700

    added sidebar buttons

--- src/pages/DashboardPage/SideBar.jsx
+++ src/pages/DashboardPage/SideBar.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 function SideBar(props) {
     return (
         <div>
-            
+            <img></img>
+            <h1>sign in user name</h1>
+            <button>Dashboard</button>
+            <button>Gradebook</button>
+            <h3>Settings</h3>
+            <button>Main Settings</button>
+
         </div>
     );
 }
