fed0b90de5a0b555861ee25f67d5406cb3b0e5b4
Andrew8bit <Andrewbith@gmail.com>
2021-06-15 20:31:12 -0700

    [component/Image] created an Image component

--- src/pages/SideBar/SideBar.jsx
+++ src/pages/SideBar/SideBar.jsx
@@ -4,13 +4,15 @@ import DashboardIcon from "../../images/dashboard.svg";
 import AssignmentIcon from "../../images/assignment.svg";
 import SettingsIcon from "../../images/settings.svg";
 
+import Image from '../../components/Image/Image';
+
 
 function SideBar(props) {
+    const caption = "user"
+    console.log(props);
+    console.log(props.currentUser);
     return (
-        <div>
-            <img></img>
-            <h1>UserName</h1>
-            
+        <div>   
             <Button variant="contained"><img src={DashboardIcon} /> Dashboard</Button>
             <Button variant="contained"><img src={AssignmentIcon} /> Gradebook</Button>
             <button>Settings</button>
